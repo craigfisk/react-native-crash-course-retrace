2205a384aa01be6f6ef9d811bd43e229028a1b17
Craig Fisk <craigfisk@picocosmos.net>
2017-07-08 15:23:06 -0700

    Switched to creating and using styles objects myView and myText in Component2.js

--- app/components/Component2/Component2.js
+++ app/components/Component2/Component2.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, Text, View, StyleSheet} from 'react-native';
 
 export default class Component2 extends Component{
   render(){
     return(
-      <View style={{backgroundColor:'#000000'}}>
-        <Text style={{color:'red'}}>Hello Craig </Text>
+      <View style={styles.myView}>
+        <Text style={styles.myText}>Hello Craig </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  myView: {
+    backgroundColor: 'blue'
+  },
+  myText: {
+    color: 'white'
+  }
+});
 
 AppRegistry.registerComponent('Component2', () => Component2);
