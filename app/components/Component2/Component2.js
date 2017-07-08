c0db09cf2be2a0b9a33083c5d6d89a4baecbd3b5
Craig Fisk <craigfisk@picocosmos.net>
2017-07-08 15:14:20 -0700

    Imported StyleSheet from react-native in Component2/Component2.js and set Text style to red

--- app/components/Component2/Component2.js
+++ app/components/Component2/Component2.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {AppRegistry, Text, View} from 'react-native';
+import {AppRegistry, Text, View, StyleSheet} from 'react-native';
 
 export default class Component2 extends Component{
   render(){
     return(
       <View>
-        <Text>Hello Craig </Text>
+        <Text style={{color:'red'}}>Hello Craig </Text>
       </View>
     );
   }
